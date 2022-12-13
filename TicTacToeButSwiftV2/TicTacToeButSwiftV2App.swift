c9ca34170e524f54e812e020e9c07325258bb71d
ItsDaBenji <benmblackmon@gmail.com>
2022-12-12 20:47:45 -0600

    Initial Commit

---
+++ TicTacToeButSwiftV2/TicTacToeButSwiftV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeButSwiftV2App.swift
+//  TicTacToeButSwiftV2
+//
+//  Created by Benjamin Blackmon on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeButSwiftV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
