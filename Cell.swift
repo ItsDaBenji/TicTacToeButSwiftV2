2cff36120e0f5e4c447a42e27dfc9a38a76fff04
ItsDaBenji <benmblackmon@gmail.com>
2022-12-12 21:56:27 -0600

    Initial Commit

---
+++ Cell.swift
@@ -0,0 +1,47 @@
+//
+//  Cell.swift
+//  TicTacToeButSwiftV2
+//
+//  Created by Benjamin Blackmon on 12/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Cell
+{
+    var tile: Tile
+    
+    func displayTile() -> String
+    {
+        switch(tile)
+        {
+        case Tile.Nought:
+            return "O"
+        case Tile.Cross:
+            return "X"
+        default:
+            return ""
+        }
+    }
+    
+    func tileColor() -> Color
+    {
+        switch(tile)
+        {
+        case Tile.Nought:
+            return Color.red
+        case Tile.Cross:
+            return Color.black
+        default:
+            return Color.black
+        }
+    }
+}
+
+enum Tile
+{
+    case Nought
+    case Cross
+    case Empty
+}
